4d12370115c51bc1edc4d97985a9ee446fd07174
Johannes Zeller <johannes_zeller@sluz.ch>
2023-05-12 10:47:47 +0200

    solve task 2

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import os
 
+from pymongo import MongoClient
+
 print(os.environ['PATH'])
+
+connection_string = os.environ['MONGODB_CONNECTION_STRING']
+client = MongoClient(connection_string)
+dbs = client.list_database_names()
+print(dbs)
